f36e9c0d62d5e6ee78843a6d60d84f94455c000d
Shubh8844 <69107302+Shubh8844@users.noreply.github.com>
2020-10-24 23:29:44 +0530

    HeroContent

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar/Navbar.js";
 import Sidebar from "../components/Sidebar/Sidebar.js";
+import Hero from "../components/Hero/Hero";
 export default function Home() {
   const [isOpen, SetisOpen] = useState(false);
 
@@ -11,6 +12,7 @@ export default function Home() {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
+      <Hero />
     </>
   );
 }
